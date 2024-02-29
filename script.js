4e2df63bfe1e29a654d805e31f4aec1b9f76cbb1
Adeoyerachael <158442459+Adeoyerachael@users.noreply.github.com>
2024-02-29 23:51:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var countdowndate = new Date("Mar 29, 2024").getTime();
+
+var x = setInterval(function(){
+
+    var now = new Date().getTime();
+
+    var distance = countdowndate - now;
+
+    var days = Math.floor(distance/(1000*60*60*24));
+    var hours = Math.floor((distance %(1000*60*60*24))/(1000*60*60));
+    var minutes = Math.floor((distance % (1000*60*60))/(1000*60));
+    var seconds = Math.floor((distance % (1000*60))/1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if(distance<0){
+        setInterval(x);
+        countdowndate = new Date(now+10*24*60*60*1000).getTime
+        document,getElementById("days").innerHTML= "10";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("minutes").innerHTML = "00";
+        document.getElementById("seconds").innerHTML = "00";
+    }
+ 
+},1000);
